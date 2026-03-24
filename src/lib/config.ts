5cb6ee7ae8c7528ad94f1a26bece7e5611d3b16e
Rohit Rajan <rohit.rajan031101@gmail.com>
2026-03-24 10:04:01 +0530

    fix: rm get url env

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -36,7 +36,7 @@ export function clearConfig(): void {
 }
 
 export function getApiUrl(): string {
-  return getConfig().apiUrl || 'https://app.maxun.dev';
+  return 'https://app.maxun.dev';
 }
 
 export function getApiKey(): string | undefined {
